0d30890cb3df8bfbb6693e2da1b4a45bce3bceb8
Arnab Dhar <arnabdhar@Arnabs-MacBook-Pro.local>
2023-06-17 18:17:27 +0100

    ENV file updated

--- client/dbClient.ts
+++ client/dbClient.ts
@@ -1,14 +1,16 @@
 import { DataSource } from "typeorm";
 import { Vehicle } from "../entity/vehicle.entity";
 import { StateLogs } from "../entity/stateLogs.entity";
+import dotenv from "dotenv";
+dotenv.config();
 
 const dataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.HOST,
   port: 5432,
-  username: "user",
-  password: "password",
-  database: "motorway",
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   entities: [Vehicle, StateLogs],
   logging: true,
 });
